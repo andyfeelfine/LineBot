28efbf8be81bd5366b147c11bb6b10179522fa35
andythinkpower <andy.huang@tpinformation.com.tw>
2023-01-02 14:37:11 +0800

    commit #7 swagger

---
+++ src/main/java/com/example/linebot/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package com.example.linebot.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "LineBot Example", version = "1.0.0"))
+public class OpenApiConfig {
+
+}
